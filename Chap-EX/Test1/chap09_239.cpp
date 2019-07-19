11770a281051851041bb55b514e0ebf9873a3932
yeonseo <yulga_nara@naver.com>
2019-07-19 16:40:03 +0900

    20190719 Exercise09

--- Chap-EX/Test1/chap09_239.cpp
+++ Chap-EX/Test1/chap09_239.cpp
@@ -8,73 +8,43 @@ chap9_239.c
 /*
 #include <stdio.h>
 void information();
-int numInput(int a, int b);
-int sum(int a, int b);
-int subtract(int a, int b);
-double multiplication(double a, double b);
+double numInput(double a, double b);
 double divide(double a, double b);
 
 int main()
 {
-	int num1=0,num2=0,result=0;
-	double res=0;
-	num1=numInput(1,100); 
-	num2=numInput(1,100);
+	double num1=0,num2=0,res=0;
+	num1=numInput(0,100); 
+	num2=numInput(0,100);
 
 	information();
 
-	result=sum(num1,num2);
-	printf("****************\n* 덧셈결과 %d\n",result);
-
-	result=subtract(num1,num2);
-	printf("****************\n* 뺄셈결과 %d\n",result);
-
-	res=multiplication(num1,num2);
-	printf("****************\n* 곱셈결과 %lf\n",res);
-
 	res=divide(num1,num2);
-	printf("****************\n* 나눗셈결과 %lf\n",res);
+	printf("나눗셈결과 %lf\n",res);
 
 	return 0;
 }
-int sum(int a, int b)
-{
-	int result;
-	result=a+b;
-	return result;
-}
-int subtract(int a, int b)
-{
-	int result;
-	result=a-b;
-	return result;
-}
-double multiplication(double a, double b)
-{
-	double result;
-	result=a*b;
-	return result;
-}
+
 double divide(double a, double b)
 {
 	double result;
 	result=a/b;
 	return result;
 }
-int numInput(int a, int b)
+double numInput(double a, double b)
 {
-	int result;
+	double result;
 	do
 	{
 		printf("수를 입력하시오 : ");
-		scanf("%d",&result);
+		scanf("%lf",&result);
 	}while(result<a||result>b);
-	printf("값이 %d로 입력되었습니다.\n",result);
+	printf("값이 %lf로 입력되었습니다.\n",result);
 	return result;
 }
 void information()
 {
-	printf("\n\n*******연산시작******\n");
+	printf("\n\n******연산시작******\n");
 	return;
 }
 //*/
