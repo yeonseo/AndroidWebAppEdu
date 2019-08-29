bd26d287b3b422ee36328e0b85472220edbd7b6c
YanSoCoder <yulga_nara@naver.com>
2019-08-29 15:20:56 +0900

    291908 java-modify

--- 201908_JAVA_Study/chapter05/src/chapter05/problem/DogTest.java
+++ 201908_JAVA_Study/chapter05/src/chapter05/problem/DogTest.java
@@ -3,7 +3,7 @@ package chapter05.problem;
 public class DogTest {
 
 	public static void main(String[] args) {
-		Dog dog = new Dog(1,"힌둥","시바");
+		Dog dog = new Dog("힌둥",1,"시바");
 		System.out.println(dog.toString());
 		System.out.println(dog.barking());
 		System.out.println(dog.hungry());
--- 201908_JAVA_Study/chapter06/src/chapter06/BoxTest.java
+++ 201908_JAVA_Study/chapter06/src/chapter06/BoxTest.java
@@ -3,8 +3,8 @@ package chapter06;
 public class BoxTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Box box = new Box();
+		Box box2 = new Box(10, 5, 10, "full");
 	}
 
 }
