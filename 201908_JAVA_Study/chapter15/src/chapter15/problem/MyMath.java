afedf14661c0182d7dd99f99fb350aebbe7f117d
YanSoCoder <yulga_nara@naver.com>
2019-09-04 17:26:10 +0900

    20190904 java-parae

--- 201908_JAVA_Study/chapter15/src/chapter15/problem/MyMath.java
+++ 201908_JAVA_Study/chapter15/src/chapter15/problem/MyMath.java
@@ -1,5 +1,12 @@
 package chapter15.problem;
 
-public class MyMath {
-
+public class MyMath<T>{
+	private T[] t;
+	public <T extends Number> double getAverage(T[] t){
+		double result = 0.0;
+		for(T i : t) {
+			result += Double.parseDouble(i + "");
+		}
+		return result/t.length;
+	}
 }
