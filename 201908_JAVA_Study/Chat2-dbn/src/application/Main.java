d8e2431588673c356e1707498b289636a2929647
YanSoCoder <yulga_nara@naver.com>
2019-09-15 23:37:22 +0900

    STUDY : JavaFX Chat-Add server func.

--- 201908_JAVA_Study/Chat2-dbn/src/application/Main.java
+++ 201908_JAVA_Study/Chat2-dbn/src/application/Main.java
@@ -9,9 +9,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 
 
 public class Main extends Application {
@@ -81,7 +86,48 @@ public class Main extends Application {
 	}
 	@Override
 	public void start(Stage primaryStage) {
-	
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
+		
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setFont(new Font("Arial",15));
+		root.setCenter(textArea);
+		
+		Button toggleButton = new Button("start");
+		toggleButton.setMaxWidth(Double.MAX_VALUE);
+		BorderPane.setMargin(toggleButton, new Insets(1,0,0,0));
+		root.setBottom(toggleButton);
+		
+		String IP = "127.0.0.1";
+		int port = 9876;
+		
+		toggleButton.setOnAction(event ->{
+			if(toggleButton.getText().equals("start")) {
+				startServer(IP, port);
+				Platform.runLater(() ->{
+					String message = String.format("server start \n",IP, port);
+					textArea.appendText(message);
+					toggleButton.setText("exit");
+					
+				});
+			}else {
+				stopServer();
+				Platform.runLater(() ->{
+					String message = String.format("server exit \n",IP, port);
+					textArea.appendText(message);
+					toggleButton.setText("start");
+					
+				});
+			}
+		});
+		
+		Scene scene = new Scene(root, 400, 400);
+		primaryStage.setTitle("Chat server");
+		primaryStage.setOnCloseRequest(event -> stopServer());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 	
 	public static void main(String[] args) {
