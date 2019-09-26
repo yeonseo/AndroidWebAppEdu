7882153d325dbe685fcb081a6c3c3a1af43a3b9d
YanSoCoder <yulga_nara@naver.com>
2019-09-26 17:57:20 +0900

    javaFX Test : view list table

---
+++ 201908_JAVA_Study/chapter10/src/controller/view_control/test/Phone.java
@@ -0,0 +1,30 @@
+package controller.view_control.test;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Phone {
+	private SimpleStringProperty smartPhone;
+	private SimpleStringProperty image;
+	public Phone(String smartPhone, String image) {
+		super();
+		this.smartPhone = new SimpleStringProperty(smartPhone);
+		this.image = new SimpleStringProperty(image);
+	}
+	public String getSmartPhone() {
+		return smartPhone.get();
+	}
+	public void setSmartPhone(String smartPhone) {
+		this.smartPhone.set(smartPhone);;
+	}
+	public String getImage() {
+		return image.get();
+	}
+	public void setImage(String image) {
+		this.image.set(image);
+	}
+	@Override
+	public String toString() {
+		return "Phone [smartPhone=" + smartPhone + ", image=" + image + "]";
+	}
+	
+}
