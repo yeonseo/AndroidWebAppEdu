c3c8c5ee3f49bfde5c1d206eba5f2a13a278d021
YanSoCoder <yulga_nara@naver.com>
2019-10-14 02:00:35 +0900

    클라이언트에서 그려지도록 연동하기 위해서 정리함

--- 201908_JAVA_Study/chatServer/src/application/draw/VertexClass.java
+++
@@ -1,9 +0,0 @@
-package application.draw;
-
-public class VertexClass {
-	double x; double y;
-	boolean draw;
-    VertexClass(double d, double e, boolean draw) {
-           this.x = d;  this.y = e; this.draw = draw;
-    }    
-}
